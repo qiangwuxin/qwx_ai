6b9bc4cc3cf2a82250e6acc73b5022243d4844a3
qiangwuxin <3148563567@qq.com>
2025-07-20 20:47:35 +0800

    useContext的学习

---
+++ react/context-demo/src/hooks/useTheme.js
@@ -0,0 +1,7 @@
+//自定义hooks 
+//hooks+component renderer
+import{useContext} from 'react';
+import{ThemeContext} from '../ThemeContext';
+export function useTheme(){
+  return useContext(ThemeContext)
+}
