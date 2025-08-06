6cd6871e3b5ab33ca636850d85ed539d02165d30
qiangwuxin <3148563567@qq.com>
2025-08-06 16:55:15 +0800

    移动端适配等项目初始化

--- batjtmd/js/str/4.js
+++ batjtmd/js/str/4.js
@@ -1,3 +1,4 @@
 let a="hello";
 console.log(a.split(''));
+console.log(a);
 console.log(typeof a);
---
+++ react/trip/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div style={{width:'2.66667rem',height:'5rem',backgroundColor:'red'}}></div>
+     <div style={{width:'3.46667rem',height:'5rem',backgroundColor:'yellow'}}></div>
+     <div className="box"></div>
+    </>
+  )
+}
+
+export default App
