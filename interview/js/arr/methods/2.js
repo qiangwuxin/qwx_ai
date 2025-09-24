8f367b97eff316fd66ed0ec38cfb98fb7ab29802
qwx <3148563567@qq.com>
2025-09-24 23:16:43 +0800

    数组的各种方法整理

---
+++ interview/js/arr/methods/2.js
@@ -0,0 +1,11 @@
+let arr=[3,1,2]; //修改原数组
+// console.log(arr.sort(),arr);
+// arr.sort((a,b)=>a-b);
+//>=0 调换 升序
+// console.log(arr.sort((a,b)=>a-b),arr);
+// console.log(arr.sort((a,b)=>b-a),arr);
+//默认字典排序
+// console.log([10,1,20,3,5].sort());
+// console.log(arr.reverse(),arr);
+//es6 
+console.log(arr.fill(0,1,3),arr);
